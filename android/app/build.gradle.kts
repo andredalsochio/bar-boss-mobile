138820a388533db5817d1996b9dfece4b7bb840b
Andre <andrefdalsochio@gmail.com>
2025-08-12 21:26:01 -0300

    chore: update dependencies for Firebase and social authentication

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,7 +28,8 @@ android {
         applicationId = "com.example.bar_boss_mobile"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        // Firebase Auth requer minSdk 23+
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName

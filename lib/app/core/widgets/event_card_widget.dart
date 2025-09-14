7648cc4ed0ea70ea2b6c03527bb6f108c06a5f72
Andre <andrefdalsochio@gmail.com>
2025-09-14 19:32:46 -0300

    refactor(event_card_widget): remove unused intl import

--- lib/app/core/widgets/event_card_widget.dart
+++ lib/app/core/widgets/event_card_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:bar_boss_mobile/app/core/constants/app_colors.dart';
 import 'package:bar_boss_mobile/app/core/constants/app_sizes.dart';
 import 'package:bar_boss_mobile/app/core/constants/app_strings.dart';
